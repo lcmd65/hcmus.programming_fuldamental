c6457d2b54f842a6d228e0a44704bf0e5120aebd
Minh-Dat Le Chon <93373784+DatMinhLeChon@users.noreply.github.com>
2022-03-13 16:54:14 +0700

    Update Source2.cpp

--- Bai3/Bai3_1/Source2.cpp
+++ Bai3/Bai3_1/Source2.cpp
@@ -148,6 +148,7 @@ void taoMotMangDong(DanhSach &list, int size){
     list.n = size;
     list.arr = new Covid19[list.n];
     for(int i= 0; i< list.n; i++){
+        cout << "Nhap thong tin benh nhan thu"<< i+1;
         themMotPhanTuDS(list.arr[i]);
     }
 }
